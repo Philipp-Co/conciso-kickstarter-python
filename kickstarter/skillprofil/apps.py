f5fbac04a4608f180749cdde29f0c71034d0b784
Philipp Kroll <philipp.kroll@conciso.de>
2025-03-10 17:26:35 +0100

    Branch fuer Vortrag vorbereitet.

--- kickstarter/skillprofil/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SkillprofilConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "skillprofil"
--- kickstarter/skillprofil/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from skillprofil.models.person import *  # noqa: F401, F403
-from skillprofil.models.skill import *  # noqa: F401, F403
